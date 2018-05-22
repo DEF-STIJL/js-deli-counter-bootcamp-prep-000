58b3181b3cfe5befd5ee6cefe27587e8b6e6f028
MATT LARSON <defstijl@gmail.com>
2018-05-22 19:35:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var katzDeli = [];
+
+takeANumber (katzDeliLine, "Ada");
+takeANumber (katzDeliLine, "Grace");
+takeANumber (katzDeliLine, "Kent");
+
+currentLine(katzDeliLine);
+
+nowServing (katzDeliLine);
+
+currentLine(katzDeliLine);
+
+takeANumber(katzDeliLine, "Matz");
+
+currentLine(katzDeliLine);
+
+nowServing (katzDeliLine);
+
+currentLine(katzDeliLine);
+
